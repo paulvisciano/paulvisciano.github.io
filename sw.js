296256e525e51229c4ed9221e5265a9ec9cba007
Paul Visciano <paulvisciano.dev@gmail.com>
2025-11-05 23:12:22 +0800

    Bump cache version to 2025.11.05.2312

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2025.10.13.0840';
+const CACHE_VERSION = '2025.11.05.2312';
 const CACHE_NAME = `whereispaul-v${CACHE_VERSION}`;
 const DYNAMIC_CACHE_NAME = `whereispaul-dynamic-v${CACHE_VERSION}`;
 
